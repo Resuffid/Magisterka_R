a754f5c2730536da380a15a4cc0061f3610cdcf6
Wiktor Warchałowski <wiktorw336@gmail.com>
2026-06-15 07:45:00 +0200

    nocka 2

--- DODATKOWYSKRYPT.R
+++ DODATKOWYSKRYPT.R
@@ -115,3 +115,236 @@ df4 <- df3 %>%
          KWS_QoL = KWS_3R+KWS_7R+KWS_12R+KWS_15R,
          KWS_G = KWS_WOD+KWS_W+KWS_JZ)
 write.csv(df4, "df4.csv")
+
+cor(df3$Class, df4$KWS_G, method ='pearson')
+cor.test(df3$Class, df4$KWS_G, method ='pearson')
+cor.test(df3$Class, df4$KWS_EES, method ='pearson')
+cor.test(df3$Class, df4$KWS_A, method ='pearson')
+cor.test(df3$Class, df4$KWS_QoL, method ='pearson')
+
+ggplot(df4, mapping = aes(x=Class, y=KWS_G))+
+  geom_jitter()+
+  geom_smooth(method = "lm")
+fit.classkwsg <- lm(Class ~ KWS_G, df4)
+summary(fit.classkwsg)
+plot(fit.classkwsg)
+
+ggplot(df4, mapping = aes(x=Class, y=KWS_EES))+
+  geom_jitter()+
+  geom_smooth(method = "lm")
+fit.classkwsees <- lm(Class ~ KWS_EES, df4)
+summary(fit.classkwsees)
+plot(fit.classkwsees)
+cohen.d(fit.classkwsees)
+
+
+ggplot(df4, mapping = aes(x=Class, y=KWS_A))+
+  geom_jitter()+
+  geom_smooth(method = "lm")
+fit.classkwsa <- lm(Class ~ KWS_A, df4)
+summary(fit.classkwsa)
+plot(fit.classkwsa)
+
+ggplot(df4, mapping = aes(x=Class, y=KWS_QoL))+
+  geom_jitter()+
+  geom_smooth(method = "lm")
+fit.classkwsqol <- lm(Class ~ KWS_QoL, df4)
+summary(fit.classkwsqol)
+plot(fit.classkwsqol)
+
+ggplot(df4, mapping = aes(x=Class, y=KWS_QoL))+
+  geom_jitter()+
+  geom_smooth(method = "lm")
+
+fit.classkwsqol <- lm(Class ~ KWS_G+KWS_EES+KWS_A+KWS_QoL, df4)
+summary(fit.classkwsqol)
+plot(fit.classkwsqol)
+
+contrasts(df4$klasa)
+df4$klasa<-as.factor(df4$Class)
+modelklasakmsg <- lm(KWS_G ~ klasa, data = df4)
+summary(modelklasakmsg)
+anova(modelklasakmsg)
+k_wielomianowe1 <- contrast(emmeans(modelklasakmsg, "klasa"), method = "poly")
+summary(k_wielomianowe1)
+ggplot(df4, aes(x = Class, y = KWS_G)) +
+  geom_boxplot(aes(group = klasa, fill = klasa), alpha = 0.4, show.legend = FALSE) +
+  
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), 
+              color = "darkred", size = 1.2, se = TRUE, alpha = 0.15) +
+  scale_x_continuous(breaks = 1:length(levels(df4$klasa)), labels = levels(df4$klasa)) +
+  theme_minimal() +
+  labs(
+    title = "Box-and-whiskers plot with trendline representing relationship betweeen general score of KWS and year of secondary school",
+    x = "Year of secondary school",
+    y = "KWS G"
+  )
+
+modelklasakwsees <- lm(KWS_EES ~ klasa, data = df4)
+summary(modelklasakwsees)
+anova(modelklasakwsees)
+k_wielomianowe2 <- contrast(emmeans(modelklasakwsees, "klasa"), method = "poly")
+summary(k_wielomianowe2)
+ggplot(df4, aes(x = Class, y = KWS_EES)) +
+  geom_boxplot(aes(group = klasa, fill = klasa), alpha = 0.4, show.legend = FALSE) +
+  
+  geom_smooth(method = "lm", formula = y ~ poly(x, 4), 
+              color = "darkred", size = 1.2, se = TRUE, alpha = 0.15) +
+  scale_x_continuous(breaks = 1:length(levels(df4$klasa)), labels = levels(df4$klasa)) +
+  theme_minimal() +
+  labs(
+    title = "Box-and-whiskers plot with trendline representing relationship betweeen EES subscale score of KWS and year of secondary school",
+    x = "Year of secondary school",
+    y = "KWS EES"
+  )
+
+
+modelklasakwsa <- lm(KWS_A ~ klasa, data = df4)
+summary(modelklasakwsa)
+anova(modelklasakwsa)
+k_wielomianowe3 <- contrast(emmeans(modelklasakwsa, "klasa"), method = "poly")
+summary(k_wielomianowe3)
+
+
+
+modelklasakwsqol <- lm(KWS_QoL ~ klasa, data = df4)
+summary(modelklasakwsqol)
+anova(modelklasakwsqol)
+k_wielomianowe4 <- contrast(emmeans(modelklasakwsqol, "klasa"), method = "poly")
+summary(k_wielomianowe4)
+ggplot(df4, aes(x = Class, y = KWS_QoL)) +
+  geom_boxplot(aes(group = klasa, fill = klasa), alpha = 0.4, show.legend = FALSE) +
+  
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), 
+              color = "darkred", size = 1.2, se = TRUE, alpha = 0.15) +
+  scale_x_continuous(breaks = 1:length(levels(df4$klasa)), labels = levels(df4$klasa)) +
+  theme_minimal() +
+  labs(
+    title = "Box-and-whiskers plot with trendline representing relationship betweeen QoL subscale score of KWS and year of secondary school",
+    x = "Year of secondary school",
+    y = "KWS G"
+  )
+
+
+
+#ANOVA
+KWS_GAOV <- aov(KWS_G ~ Living, data = df4)
+KWS_GAOV
+summary(KWS_GAOV)
+TukeyHSD(KWS_GAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=KWS_G)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="KWS G")
+eta_squared(KWS_GAOV)
+
+KWS_EESAOV <- aov(KWS_EES ~ Living, data = df4)
+KWS_EESAOV
+summary(KWS_EESAOV)
+TukeyHSD(KWS_EESAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=KWS_EES)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="KWS EES")
+eta_squared(KWS_EESAOV)
+
+KWS_AAOV <- anova(lm(KWS_A ~ Living, data = df4))
+KWS_AAOV
+eta_squared(KWS_AAOV)
+
+KWS_QoLAOV <- aov(KWS_QoL ~ Living, data = df4)
+KWS_QoLAOV
+summary(KWS_QoLAOV)
+TukeyHSD(KWS_QoLAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=KWS_QoL)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="KWS QoL")
+eta_squared(KWS_QoLAOV)
+
+
+FRAS_FCPSAOV <- aov(FRAS_FCPS ~ Living, data = df4)
+FRAS_FCPSAOV
+summary(FRAS_FCPSAOV)
+TukeyHSD(FRAS_FCPSAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_FCPS)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS FCPS")
+eta_squared(FRAS_FCPSAOV)
+
+FRAS_FCPSAOV <- aov(FRAS_FCPS ~ Living, data = df4)
+FRAS_FCPSAOV
+summary(FRAS_FCPSAOV)
+TukeyHSD(FRAS_FCPSAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_FCPS)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS FCPS")
+
+FRAS_USERAOV <- aov(FRAS_USER ~ Living, data = df4)
+FRAS_USERAOV
+summary(FRAS_USERAOV)
+TukeyHSD(FRAS_USERAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_USER)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS USER")
+
+FRAS_MPOAOV <- aov(FRAS_MPO ~ Living, data = df4)
+FRAS_MPOAOV
+summary(FRAS_MPOAOV)
+TukeyHSD(FRAS_MPOAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_MPO)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS MPO")
+
+FRAS_FCAOV <- aov(FRAS_FC ~ Living, data = df4)
+FRAS_FCAOV
+summary(FRAS_FCAOV)
+TukeyHSD(FRAS_FCAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_FC)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS FC")
+
+FRAS_FSAOV <- aov(FRAS_FS ~ Living, data = df4)
+FRAS_FSAOV
+summary(FRAS_FSAOV)
+TukeyHSD(FRAS_FSAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_FS)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS FS")
+
+FRAS_AMMAAOV <- aov(FRAS_AMMA ~ Living, data = df4)
+FRAS_AMMAAOV
+summary(FRAS_AMMAAOV)
+TukeyHSD(FRAS_AMMAAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_AMMA)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS AMMA")
+
+FRAS_ALLAOV <- aov(FRAS_ALL ~ Living, data = df4)
+FRAS_ALLAOV
+summary(FRAS_ALLAOV)
+TukeyHSD(FRAS_ALLAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=FRAS_ALL)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="FRAS ALL")
+
+JiMSLAOV <- aov(JiMSz_L ~ Living, data = df4)
+JiMSLAOV
+summary(JiMSLAOV)
+TukeyHSD(JiMSLAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=JiMSz_L)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="JiMSz A")
+
+JiMSMAOV <- aov(JiMSz_M ~ Living, data = df4)
+JiMSMAOV
+summary(JiMSMAOV)
+TukeyHSD(JiMSMAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=JiMSz_M)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="JiMSz M")
+
+JiMSKAOV <- aov(JiMSz_K ~ Living, data = df4)
+JiMSKAOV
+summary(JiMSKAOV)
+TukeyHSD(JiMSKAOV)
+ggplot(data=df4, mapping = aes(x=Living, y=JiMSz_K)) +
+  geom_boxplot()+
+  labs(x="Place of Living", y="JiMSz L")
